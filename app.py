b20c612bcdfaa3ab8743e8e1641c61db2df4d1d3
Ankur820 <cankur965@gmail.com>
2025-08-11 21:33:58 +0530

    add CORS

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify
 from translate import Translator
 
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app) 
 
 @app.route('/translate', methods=['POST'])
 def translate_text():
@@ -30,4 +33,5 @@ def translate_text():
     
 
 if __name__ == '__main__':
+
     app.run(debug=True,port=8000,host='0.0.0.0')
